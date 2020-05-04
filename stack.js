871ab4a336941192dd43ae9a317b5e7f72a84067
questcoin <iopell2007@gmail.com>
2020-05-04 21:58:40 +0900

    Add stack

---
+++ stack.js
@@ -0,0 +1,38 @@
+class StackOverflowError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "StackOverflowError";
+    }
+}
+
+class Stack {
+    #datas;
+
+    constructor() {
+        this.#datas = [];
+        this.top = -1;
+        this.length = 0;
+    }
+
+    push(element) {
+        if(this.length === Number.MAX_SAFE_INTEGER) {
+            throw new StackOverflowError("stack size exceeds javascript's internal safe limit");
+        }
+        this.top++;
+        this.#datas[this.top] = element;
+        this.length++;
+    }
+
+    pop() {
+        if(this.length <= 0) return null;
+        const ret = this.#datas.pop();
+        this.top--;
+        this.length--;
+        return ret;
+    }
+
+    get(index) {
+        if(index > this.top) return null;
+        return this.#datas[index];
+    }
+}
